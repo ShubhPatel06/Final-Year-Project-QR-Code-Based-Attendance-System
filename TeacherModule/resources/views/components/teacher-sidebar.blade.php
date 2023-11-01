704d1d1a41dcc0d904993e3fba85c9d75cfcd4fb
Shubh Patel <shubhpatel891@gmail.com>
2023-11-01 18:13:30 +0300

    Worked on responsiveness

--- TeacherModule/resources/views/components/teacher-sidebar.blade.php
+++ TeacherModule/resources/views/components/teacher-sidebar.blade.php
@@ -2,20 +2,68 @@
     <div class="flex flex-col gap-y-2 mt-2 items-center w-full text-sm my-8 px-8">
 
         <div class="{{ $focus=='dashboard' ? 'bg-blue-500 text-white px-6' : 'text-black' }} flex flex-row h-12 items-center space-x-4 text-sm mx-4 lg:text-base rounded drop-shadow">
+            <i class="fa-solid fa-house"></i>
             <p class=""><a href="">Dashboard</a></p>
-
         </div>
 
         <div class="{{ $focus=='classes' ? 'bg-blue-500 text-white px-6' : 'text-black' }} flex flex-row h-12 items-center space-x-4 text-sm mx-4 lg:text-base rounded drop-shadow">
+            <i class="fa-solid fa-list"></i>
             <p class=""><a href="">Classes</a></p>
-
         </div>
 
         <div class="{{ $focus=='attendance' ? 'bg-blue-500 text-white px-6' : 'text-black' }} flex flex-row h-12 items-center space-x-4 text-sm mx-4 lg:text-base rounded drop-shadow">
+            <i class="fa-solid fa-clipboard-user"></i>
             <p class=""><a href="">Attendance</a></p>
-
         </div>
 
     </div>
 
 </div>
+
+<div id="mobile-nav" class="pt-5 z-40 hidden w-[60%] top-0 left-0 md:hidden fixed h-[100vh] bg-slate-500">
+    <div id="close-nav" class="flex text-2xl md:hidden absolute top-6 left-3 hover:cursor-pointer mb-4">
+        <i class="fa-solid fa-xmark"></i>
+    </div>
+    <div class="flex flex-col gap-4 items-start text-blue-strath w-full text-sm mt-10 px-5">
+        <div class="{{$focus == 'dashboard' ? 'bg-blue-500 text-white':'hover:font-semibold' }} flex p-4 h-10 justify-center items-center gap-3 text-sm rounded-md">
+            <i class="fa-solid fa-house"></i>
+            <p class=""><a href="">Dashboard</a></p>
+        </div>
+        <div class="{{$focus == 'dashboard' ? 'bg-blue-500 text-white':'hover:font-semibold' }} flex p-4 h-10 justify-center items-center gap-3 text-sm rounded-md">
+            <i class="fa-solid fa-list"></i>
+            <p class=""><a href="">Classes</a></p>
+        </div>
+        <div class="{{$focus == 'dashboard' ? 'bg-blue-500 text-white':'hover:font-semibold' }} flex p-4 h-10 justify-center items-center gap-3 text-sm rounded-md">
+            <i class="fa-solid fa-clipboard-user"></i>
+            <p class=""><a href="">Attendance</a></p>
+        </div>
+    </div>
+</div>
+
+<div id="mobile-toggle" class="fixed md:hidden text-white left-3 z-50 w-14 h-10 top-3 rounded-md flex justify-center shadow-md items-end bg-blue-500 p-2 toggle-button">
+    @switch($focus)
+    @case('dashboard')
+    <i class="fa-solid fa-house"></i>
+    @break
+
+    @default
+    @endswitch
+
+</div>
+
+<script>
+    $(document).ready(function() {
+        const $toggleButton = $(".toggle-button");
+
+        $("#mobile-toggle").click(function(e) {
+            $("#mobile-nav").slideDown();
+            $toggleButton.hide();
+        });
+
+        $("#close-nav").click(function(e) {
+            $("#mobile-nav").slideUp();
+            $toggleButton.show();
+        });
+
+    });
+</script>
--- TeacherModule/resources/views/layouts/main.blade.php
+++ TeacherModule/resources/views/layouts/main.blade.php
@@ -6,7 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Teacher Module</title>
     <script src="https://cdn.tailwindcss.com"></script>
-
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://kit.fontawesome.com/d3e0a0693f.js" crossorigin="anonymous"></script>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
@@ -17,21 +18,20 @@
     @php
     $user = Auth::User();
     @endphp
-    <nav class="fixed flex top-0 w-full h-16 z-50 border-b-2 justify-between items-center px-11">
-        <div class="text-black md:flex flex-col justify-center pl-3">
-            <P class="text-xl text-white">Attendance System</P>
-        </div>
 
-        <div class="flex items-center gap-x-3 h-full">
-            <span class="text-black">{{$user->first_name. " ".$user->last_name}}</span>
-            <div>
-                <form action="{{ route('logout') }}" method="POST">
-                    @csrf
-                    <button type="submit" class="ml-3 text-red-500 hover:underline"> Logout </button>
-                </form>
-            </div>
+    <nav class="fixed md:z-50 top-0 w-full h-16 border-b-2 flex md:justify-between justify-end items-center px-8">
+
+        <div class="text-2xl text-white md:block hidden">Attendance System</div>
+
+        <div class="flex items-center gap-4 text-lg">
+            <span>{{ $user->first_name }} {{ $user->last_name }}</span>
+            <form action="{{ route('logout') }}" method="POST">
+                @csrf
+                <button type="submit" class="text-red-500 hover:text-red-700 hover:underline">Logout</button>
+            </form>
         </div>
     </nav>
+
 
     <!-- @yield('layoutContent') -->
     <div class="mt-16">
